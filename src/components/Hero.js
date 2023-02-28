c91fa5d8a4dd4325523ff4df19f3e54c21878c81
Daniele Manca <danimanca2010@gmail.com>
2023-02-28 18:14:42 +0000

    Added profile image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './hero.scss'
+import profilepicture from '../assets/profile_photo.png'
 
 const Hero = () => {
   return (
     <section className="hero">
       <div className="hero__content">
         <div className="hero__content__image">
-          <p>My Image</p>
+          <img src={profilepicture} alt="" />
         </div>
         <div className="hero__content__text">
           <h1>Front-End Engineer</h1>
